cbf7b183366d562c861df365ee2d013e6f53f9f2
stars under <stars.under@Qq.com>
2022-04-29 09:27:21 +0800

    生成了Resele,并将Resele文件夹剔除同步

--- resele/main.d
+++ resele/main.d
@@ -1,1 +1,4 @@
-resele/main.o: main.cpp tools/include/TcpServer.h
+resele/main.o: main.cpp tools/include/TcpServer.h \
+ tools/include/SyncTools/textSync.h tools/include/SyncServer.h \
+ tools/include/TcpServer.h tools/include/SyncTools/TemporaryData.h \
+ tools/include/SyncTools/TemporaryData.h
